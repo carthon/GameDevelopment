2176ff896b3100dcba0ff9a1092f0f52efcd8f17
Daniel Rello <danielrello@gmail.com>
2022-05-18 14:17:28 +0200

    Added inventory System

--- Assets/_Project/Scripts/Components/Locomotion.cs
+++ Assets/_Project/Scripts/Components/Locomotion.cs
@@ -1,58 +1,152 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using TMPro.SpriteAssetUtilities;
+﻿using System;
+using System.Runtime;
+using _Project.Scripts.Handlers;
+using Cinemachine.Editor;
+using StarterAssets;
 using UnityEngine;
-using UnityEngine.Diagnostics;
 
 namespace _Project.Scripts.Components {
-    [RequireComponent(typeof(Rigidbody))]
     public class Locomotion : MonoBehaviour {
-        [Range(0,100)]
-        [SerializeField] float maxSpeed = 5f;
-        [Range(0,100)]
-        [SerializeField] float maxRunSpeed = 10f;
-        [Range(0,100)]
-        [SerializeField] float maxAcceleration = 4f;
-        [Range(0,100)]
-        [SerializeField] float jumpStrength = 4f;
-        Vector2 direction;
-        private Vector3 desiredVelocity;
-        private bool desiredJump;
-        private Vector3 velocity;
-        private Rigidbody body;
+        private InputHandler inputHandler;
 
-        private bool _isGrounded;
-        // Start is called before the first frame update
-        void Awake() {
-            body = GetComponent<Rigidbody>();
+        [HideInInspector]
+        public Transform myTransform;
+        [HideInInspector]
+        public AnimatorHandler animatorHandler;
+
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+
+        public bool isSprinting;
+        public bool isInteracting;
+        public bool isRolling;
+        public bool isGrounded;
+
+        [Header("Ground & Air detection Stats")]
+        [SerializeField] private float groundDetectionRayStartPoint = .5f;
+        [SerializeField] private float minimumDistanceNeededToBeginFall = 1f;
+        [SerializeField] private float groundDirectionRayDistance = .2f;
+        [SerializeField] private float groundDistance = .2f;
+        [SerializeField] private LayerMask ignoreForGroundCheck;
+
+        [Header("Stats")]
+        [SerializeField] private float movementSpeed = 5f;
+        [SerializeField] private float sprintSpeed = 7f;
+        [SerializeField] private float rotationSpeed = 10f;
+        [SerializeField] private float inAirMovementControl = 10f;
+        [SerializeField] private float fallingSpeed = 45f;
+        private float moveAmount;
+        [SerializeField] private Vector3 moveDirection;
+
+        private void Start() {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            myTransform = transform;
+            animatorHandler = GetComponentInChildren<AnimatorHandler>();
+            animatorHandler.Initialize();
+            isGrounded = true;
+            //ignoreForGroundCheck = ~(1 << 8 | 1 << 11);
         }
 
-        // Update is called once per frame
-        void FixedUpdate() {
-            MoveBody();
-            if (desiredJump && _isGrounded) {
-                desiredJump = false;
-                Jump();
+        #region Movement
+
+        private Vector3 normalVector;
+        private Vector3 targetPosition;
+
+        private void HandleRotation(float delta, Vector3 targetDir) {
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+                targetDir = myTransform.forward;
+
+            float rs = rotationSpeed;
+            
+            Quaternion tr = Quaternion.LookRotation(targetDir);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+        #endregion
+        
+        private static readonly int IsInteracting = Animator.StringToHash("isInteracting");
+        public void HandleMovement(float delta, Vector3 moveDirection) {
+            this.moveDirection = moveDirection;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+            if (isRolling || !isGrounded)
+                return;
+            moveDirection.Normalize();
+            moveDirection.y = 0;
+
+            float speed = movementSpeed;
+            if (isSprinting) {
+                speed = sprintSpeed;
+            }
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.velocity = projectedVelocity;
+
+            animatorHandler.UpdateAnimatorValues(moveAmount, 0, isSprinting);
+            
+            if (animatorHandler.canRotate)
+                HandleRotation(delta, moveDirection);
+        }
+
+        public void HandleRollingAndSprinting(float delta, Vector3 moveDirection) {
+            isInteracting = animatorHandler.animator.GetBool(IsInteracting);
+            if (isInteracting)
+                return;
+
+            if (isRolling) {
+                if (moveAmount > 0) {
+                    animatorHandler.PlayTargetAnimation("Rolling", true, 1);
+                    moveDirection.y = 0;
+                    Quaternion rollRotation = Quaternion.LookRotation(moveDirection);
+                    myTransform.rotation = rollRotation;
+                }
+                else {
+                    //animatorHandler.PlayTargetAnimation("Backstep", true);
+                }
             }
         }
-        void MoveBody() {
-            velocity = body.velocity;
-            float maxSpeedChange = maxAcceleration * Time.fixedDeltaTime;
-            Vector3 desiredVelocity = new Vector3(direction.x, 0f, direction.y) * maxSpeed;
-            velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-            velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-            body.velocity = velocity;
-        }
 
-        void Jump() {
-            float jumpValue = jumpStrength;
-            velocity.y += jumpValue;
-            body.velocity = velocity;
+        public void HandleFalling(float delta, Vector3 moveDirection) {
+            RaycastHit hit;
+            Vector3 origin = myTransform.position;
+            origin.y += groundDetectionRayStartPoint;
+            moveDirection.y = 0;
+
+            if (!isGrounded) {
+                rigidbody.AddForce(-Vector3.up * fallingSpeed);
+            }
+            Vector3 dir = moveDirection;
+            dir.Normalize();
+            origin += dir * groundDirectionRayDistance;
+
+            targetPosition = myTransform.position;
+            
+            Debug.DrawRay(origin, -Vector3.up * minimumDistanceNeededToBeginFall, Color.red, 0.1f, false);
+            if (Physics.Raycast(origin, -Vector3.up, out hit, minimumDistanceNeededToBeginFall, ignoreForGroundCheck)) {
+                normalVector = hit.normal;
+                Vector3 tp = hit.point;
+                targetPosition.y = tp.y + groundDistance;
+                isGrounded = true;
+            }else if(isGrounded)
+                isGrounded = false;
+            if (!animatorHandler.animator.GetBool("isFalling") && !isGrounded)
+                animatorHandler.PlayTargetAnimation("Falling", false, 1);
+            if (!isGrounded) {
+                Vector3 vel = rigidbody.velocity.normalized;
+                vel *= (movementSpeed) / inAirMovementControl; 
+                rigidbody.velocity.Set(vel.x, rigidbody.velocity.y, vel.z);
+                animatorHandler.animator.SetFloat("FallingSpeed", rigidbody.velocity.magnitude);
+            }
+            if (isGrounded) {
+                myTransform.position = targetPosition;
+            }
+            animatorHandler.animator.SetBool("isFalling", !isGrounded);
         }
-        public void SetGrounded(bool grounded) => _isGrounded = grounded;
-        public void TriggerJump(bool jump) => desiredJump |= jump;
-        public void SetDirection(Vector2 dir) => direction = dir;
     }
 }
--- Assets/_Project/Scripts/Utils/Utilities.cs
+++ Assets/_Project/Scripts/Utils/Utilities.cs
@@ -1,9 +1,126 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace _Project.Scripts.Utils {
     public static class Utilities {
+        /// <summary>
+        /// Rotates around a given point with a given angle
+        /// </summary>
+        /// <param name="target">Transform target that will pivot around</param>
+        /// <param name="pivotPoint">Point in the world to pivot</param>
+        /// <param name="angle">Angle to pivot with</param>
         public static void RotateAround(Transform target, Vector3 pivotPoint, float angle) {
             target.position = angle * (target.position - pivotPoint) + pivotPoint;
         }
+        /// <summary>
+        /// Returns the angle clamped between a max value and a min value
+        /// </summary>
+        /// <param name="lfAngle">Angle to clamp</param>
+        /// <param name="lfMin">Min value of the angle</param>
+        /// <param name="lfMax"> Max value of the angle</param>
+        /// <returns></returns>
+        public static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+        {
+            if (lfAngle < -360f) lfAngle += 360f;
+            if (lfAngle > 360f) lfAngle -= 360f;
+            return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        }
+        /// <summary>
+        /// Calculates the normalized projection of the Vector3 'vec'
+        /// onto the horizontal plane defined by the orthogonal vector (0, 1, 0)
+        /// </summary>
+        /// <param name="vec">The vector to project</param>
+        /// <returns>The normalized projection of 'vec' onto the horizontal plane</returns>
+        public static Vector3 GetFloorProjection(in Vector3 vec) {
+            return Vector3.ProjectOnPlane(vec, Vector3.up).normalized;
+        }
     }
-}
+    [Serializable]
+    public class BodyPart {
+            public string bodyPartName;
+
+            [SerializeField] private List<ConfigurableJoint> _joints;
+            private List<JointDriveConfig> XjointDriveConfigs;
+            private List<JointDriveConfig> YZjointDriveConfigs;
+
+            [SerializeField] private float _strengthScale = 1;
+            public float StrengthScale { get { return _strengthScale; } }
+
+            public BodyPart(string name, List<ConfigurableJoint> joints) {
+                bodyPartName = name;
+                _joints = joints;
+            }
+
+            public void Init() {
+                XjointDriveConfigs = new List<JointDriveConfig>();
+                YZjointDriveConfigs = new List<JointDriveConfig>();
+
+                foreach (ConfigurableJoint joint in _joints) {
+                    XjointDriveConfigs.Add((JointDriveConfig) joint.angularXDrive);
+                    YZjointDriveConfigs.Add((JointDriveConfig) joint.angularYZDrive);
+                }
+                _strengthScale = 1;
+            }
+        }
+    }
+    [Serializable]
+    public class JointDriveConfig {
+        [SerializeField] private float _positionSpring, _positionDamper, _maximumForce;
+        public float PositionSpring { get { return _positionSpring; } }
+        public float PositionDamper { get { return _positionDamper; } }
+        public float MaximumForce { get { return _maximumForce; } }
+
+        public static explicit operator JointDrive(JointDriveConfig config) {
+            JointDrive jointDrive = new JointDrive {
+                positionSpring = config._positionSpring,
+                positionDamper = config._positionDamper,
+                maximumForce = config._maximumForce
+            };
+            return jointDrive;
+        }
+        public static explicit operator JointDriveConfig(JointDrive jointDrive) {
+            JointDriveConfig jointDriveConfig = new JointDriveConfig {
+                _positionSpring = jointDrive.positionSpring,
+                _positionDamper = jointDrive.positionDamper,
+                _maximumForce = jointDrive.maximumForce
+            };
+            return jointDriveConfig;
+        }
+
+        public readonly static JointDriveConfig ZERO = new JointDriveConfig {
+            _positionSpring = 0, _positionDamper = 0, _maximumForce = 0
+        };
+        public static JointDriveConfig operator *(JointDriveConfig config, float multiplier) {
+            return new JointDriveConfig {
+                _positionSpring = config._positionSpring * multiplier,
+                _positionDamper = config._positionDamper * multiplier,
+                _maximumForce = config._maximumForce * multiplier,
+            };
+        }
+        [Serializable]
+        public struct JointMotionsConfig {
+            public ConfigurableJointMotion angularXMotion, angularYMotion, angularZMotion;
+            public float angularXLimit, angularYLimit, angularZLimit;
+
+            public void ApplyTo(ref ConfigurableJoint joint) {
+                joint.angularXMotion = angularXMotion;
+                joint.angularYMotion = angularYMotion;
+                joint.angularZMotion = angularZMotion;
+
+                var softJointLimit = new SoftJointLimit();
+                
+                softJointLimit.limit = angularXLimit / 2;
+                joint.highAngularXLimit = softJointLimit;
+
+                softJointLimit.limit = -softJointLimit.limit;
+                joint.lowAngularXLimit = softJointLimit;
+
+                softJointLimit.limit = angularYLimit;
+                joint.angularYLimit = softJointLimit;
+
+                softJointLimit.limit = angularZLimit;
+                joint.angularZLimit = softJointLimit;
+            }
+        }
+    }
